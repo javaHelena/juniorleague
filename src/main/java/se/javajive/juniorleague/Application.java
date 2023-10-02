cdfde7dc55895cf0519b65924e6d9b1f56689d07
Helena Hjertén <javaHelena>
2023-10-02 12:30:55 +0000

    Initial commit

---
+++ src/main/java/se/javajive/juniorleague/Application.java
@@ -0,0 +1,10 @@
+package se.javajive.juniorleague;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
